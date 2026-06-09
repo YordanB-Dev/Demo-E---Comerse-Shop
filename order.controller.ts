883217e1092bfb3720f28dcc5026bf810b525001
Yordan Borisov <67415791+YordanB-Dev@users.noreply.github.com>
2026-06-09 11:57:52 +0200

    Add files via upload

---
+++ order.controller.ts
@@ -0,0 +1,29 @@
+import type { Request, Response, NextFunction } from "express";
+import orderService from "../services/order.service.js";
+import asyncHandler from "../middleware/asyncHandler.js";
+
+ 
+export const orderController = {
+    createOrder: asyncHandler(async (req: Request, res: Response) => {
+        const userId = (req as any).user.id;
+        const { items } = req.body;
+        const order = await orderService.createOrder({userId, items});
+
+        return res.status(201).json(order);
+    }),
+
+    getByUserId: asyncHandler(async(req: Request, res: Response) => {
+        const userId = (req as any).user.id;
+        const order = await orderService.getByUserId(userId);
+        return res.status(200).json(order);
+    }),
+
+    getOrderById: asyncHandler(async(req: Request, res: Response) => {
+        const orderId = Number (req.params.id);
+        const userId = (req as any).user.id
+        const order = await orderService.getByID(orderId, userId);
+        return res.status(200).json(order);
+    }),
+};
+
+export default orderController;
