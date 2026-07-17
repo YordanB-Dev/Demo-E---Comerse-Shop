2fbb4f1c8ddfdacb37446895e8b5397bd9a0f6f3
Yordan Borisov <67415791+YordanB-Dev@users.noreply.github.com>
2026-07-17 12:52:13 +0200

    Fix typos in cart service methods and interfaces

--- cart.serivce.ts
+++ cart.serivce.ts
@@ -2,6 +2,7 @@ import cartRepository from "../repositories/cart.repository.js";
 import productRepository from "../repositories/product.repository.js";
 import { AppError } from "../middleware/types/AppError.js";
 
+
 interface CartItem {
     id: number;
     cart_id: number;
@@ -17,15 +18,16 @@ interface CartResult {
     total: number;
 }
 
+
 export const cartSerivce = {
     async getCart(userId: number): Promise<CartResult> {
-        const cart = await cartRepository.getOrCreateCart(userId);
-        const items = await cartRepository.getOrCreateCart(cart.id) as unknown as CartItem[];
+        const cart = await cartRepository.getOrCreatCart(userId);
+        const items = await cartRepository.getOrCreatCart(cart.id) as unknown as CartItem[];
 
         return {
             cartId: cart.id,
             items,
-            total: items.reduce((sum, item) => sum + item.price * item.quantity, 0)
+            total: items.reduce((sum, item) => sum + item.price * item.quantity, 0),
         }
     },
 
@@ -39,23 +41,23 @@ export const cartSerivce = {
             throw new AppError(`Product not found`, 404);
         }
 
-        if (product.stock.quantiy < quantity) {
+        if (product.stock.quantity < quantity) {
             throw new AppError(`Not enough amount for this product`, 400);
         }
 
-        const cart = await cartRepository.getOrCreateCart(userId);
-        await cartRepository.addItem(userId, productId, quantity);
+        const cart = await cartRepository.getOrCreatCart(userId);
+        await cartRepository.removeItem(userId, productId, quantity);
         return this.getCart(userId);
     },
     
     async removeItem(userId: number, productId: number, quantity: number) {
-        const cart = await cartRepository.getOrCreateCart(userId);
+        const cart = await cartRepository.getOrCreatCart(userId);
         await cartRepository.removeItem(userId, productId, quantity);
         return this.getCart(userId);
     },
 
     async clearCart(userId: number) {
-        const cart = await cartRepository.getOrCreateCart(userId);
+        const cart = await cartRepository.getOrCreatCart(userId);
         await cartRepository.clearCart(userId);
         return {message: `Cart is clear`};
     }
