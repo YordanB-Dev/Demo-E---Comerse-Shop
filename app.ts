2cf7fc7b39e639c275afe866b791b78c866ebfa8
Yordan Borisov <67415791+YordanB-Dev@users.noreply.github.com>
2026-07-17 12:53:45 +0200

    Integrate orderRouter for order API endpoints
    Added orderRouter to handle order-related API routes.

--- app.ts
+++ app.ts
@@ -3,12 +3,15 @@ import type { Request, Response} from "express";
 
 import taskRoutes from "./routes.js";
 import errorHandler from "./middleware/errorHandler.js";
+import orderRouter from "./routes.js";
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 
 app.use("/api", taskRoutes);
+
+app.use("/api/orders", orderRouter);
 
 app.use("/", (req: Request, res: Response) => {
     res.send("Welcome to backend server");
